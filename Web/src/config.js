8b702a9d807a67c272b2c582dc9531d1943bbc68
andreasmarkovic712 <andreasmarkovic712@gmail.com>
2021-07-07 16:43:17 +0200

    update config

--- Web/src/config.js
+++ Web/src/config.js
@@ -1,8 +1,8 @@
 let config = {};
-config.sm_bridge = '0x00d8D2A501955a410710091F693Eec765e5A4214';
+config.sm_bridge = '0x6F26373521cc771250AE18eA5794676E1552c5ba';
 config.data_path = '/home';
-config.gas='0x81B320';
-config.gasPrice = '0x4A817C800';
+config.gas='0x21CF8';
+config.gasPrice = '0x12A05F200';
 config.password = 'utYTY4#ll'
 config.api="http://localhost:3000";
 config.start_common_amounts = 10;
